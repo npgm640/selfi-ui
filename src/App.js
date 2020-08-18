23e792565cb5942b8ec55e4ae900a3311c909d8a
ranga <bethapudiv@gmail.com>
2020-08-17 20:49:09 -0400

    ui navigation

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/navigation/MainNavigation";
+
+const App = () => {
+    return (
+        <Router>
+            <MainNavigation/>
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <Users/>
+                    </Route>
+                    <Route path="/places/new" exact>
+                        <NewPlace/>
+                    </Route>
+                    <Redirect to="/"/>
+                </Switch>
+            </main>
+        </Router>
+    );
+};
+
+export default App;
